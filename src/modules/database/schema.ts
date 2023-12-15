3d755c5218cda6c9df13052339e1d77058d57b8a
Fabrício Souza <banksapk@gmail.com>
2023-12-15 15:12:02 -0300

    edited schema consulta

--- src/modules/database/schema.ts
+++ src/modules/database/schema.ts
@@ -7,9 +7,9 @@ import { StatusMedico } from "../domain/medico/medico.interface";
 
 
 const medicoSchema = new Schema({
-  id: {type: String, required: true},
+  id: {type: String, required: true, unique: true},
   nome: {type: String, required: true},
-  CRM: {type: String, required: true},
+  CRM: {type: String, required: true, unique: true},
   especialidade: { type: String, required: true },
   endereco: [{
     estado: {type: String, required: true},
@@ -26,9 +26,9 @@ const medicoSchema = new Schema({
 
 
 const pacienteSchema = new Schema({
-  id: {type: String, required: true},
+  id: {type: String, required: true, unique: true},
   nome: {type: String, required: true},
-  CPF: {type: String, required: true},
+  CPF: {type: String, required: true, unique: true},
   endereco: [{
     estado: {type: String, required: true},
     cidade: {type: String, required: true},
@@ -43,38 +43,8 @@ const pacienteSchema = new Schema({
 
 const consultaSchema = new Schema({
   id: { type: String, required: true },
-  paciente:{
-    id: {type: String, required: true},
-    nome: {type: String, required: true},
-    idade: {type: Number, required: true},
-    CPF: {type: String, required: true},
-    endereco:{
-      estado: {type: String, required: true},
-      cidade: {type: String, required: true},
-      cep: {type: String, required: true},
-      bairro: {type: String, required: true},
-      rua: {type: String, required: true},
-      numero: {type: String, required: true}
-    },
-    telefone: {type: String, required: true}
-  },
-  medico: {
-    id: {type: String, required: true},
-    nome: {type: String, required: true},
-    idade: {type: Number, required: true},
-    CRM: {type: String, required: true},
-    endereco:{
-      estado: {type: String, required: true},
-      cidade: {type: String, required: true},
-      cep: {type: String, required: true},
-      bairro: {type: String, required: true},
-      rua: {type: String, required: true},
-      numero: {type: String, required: true}
-    },
-    especialidade: {type: String, required: true},
-    status: {type: String, enum: StatusMedico, required: true},
-    telefone: {type: String, required: true}
-  },
+  paciente: { type: Schema.Types.ObjectId, ref: 'Paciente', required: true },
+  medico: { type: Schema.Types.ObjectId, ref: 'Medico', required: true },
   data: { type: Date, required: true },
   valor: { type: Number, required: true },
   status_do_pagamento: { type: String, enum: paymentStatus, required: true },
